94c811caa6cbda943a729aef6c67cb42acde2ddf
Matheus Borges <matob@live.com>
2020-04-10 22:18:19 -0300

    Alteração de DY para valor real

--- src/main/java/dev/matob/scraping/founds_explorer/Main.java
+++ src/main/java/dev/matob/scraping/founds_explorer/Main.java
@@ -23,8 +23,10 @@ public class Main {
 				List<String> _result = new LinkedList<>();
 				_driver.setHeadless(true);
 
-				for (int i = 0; i < urls.size(); i++)
+				for (int i = 0; i < urls.size(); i++) {
 					_result.add(execute(_driver, urls.get(i)));
+					System.out.println(_result.get(i));
+				}
 
 				synchronized (result) {
 					result.addAll(_result);
@@ -59,17 +61,17 @@ public class Main {
 			driver.findE(e, By.className("indicator-value"));
 			value = driver.getText();
 
-			if ("Liquidez Diária".equals(label))
+			if ("Liquidez DiÃ¡ria".equals(label))
 				negociacoes = !"N/A".equals(value) ? value.replace(".", "") : "0";
-			else if ("Último Rendimento".equals(label))
+			else if ("Ãšltimo Rendimento".equals(label))
 				dividendo = !"N/A".equals(value) ? value.replace("R$ ", "") : "0";
-			else if ("Patrimônio Líquido".equals(label)) {
+			else if ("PatrimÃ´nio LÃ­quido".equals(label)) {
 				valorPatr = !"N/A".equals(value) ? value.replace("R$ ", "") : "0";
 				if (valorPatr.endsWith("mi"))
 					valorPatr = String.format("%.2f", (Double.parseDouble(valorPatr.replace(",", ".").replace(" mi", "")) * 1000000));
 				if (valorPatr.endsWith("bi"))
 					valorPatr = String.format("%.2f", (Double.parseDouble(valorPatr.replace(",", ".").replace(" bi", "")) * 1000000000));
-			} else if ("Rentab. no mês".equals(label))
+			} else if ("Rentab. no mÃªs".equals(label))
 				rentMensal = !"N/A".equals(value) ? value : "0";
 		}
 
@@ -85,23 +87,30 @@ public class Main {
 			}
 		}
 
+		double pr1 = 0, pr3 = 0, pr6 = 0, pr12 = 0;
 		double dy1 = 0, dy3 = 0, dy6 = 0, dy12 = 0;
 		driver.findD(By.id("dividends"));
 		driver.findE(By.className("table"));
 		driver.findAllE(By.tagName("tr"));
 		List<WebElement> labels = driver.getElements(0).findElements(By.tagName("th"));
-		List<WebElement> values = driver.getElements(2).findElements(By.tagName("td"));
-		for (int x = 0; x < labels.size() && x < values.size(); x++) {
-			if ("Último".equals(labels.get(x).getText()))
-				dy1 = Double.parseDouble(values.get(x).getText().replace("%", "").replace(",", "."));
-			else if ("3 meses".equals(labels.get(x).getText()))
-				dy3 = Double.parseDouble(values.get(x).getText().replace("%", "").replace(",", ".")) / 3;
-			else if ("6 meses".equals(labels.get(x).getText()))
-				dy6 = Double.parseDouble(values.get(x).getText().replace("%", "").replace(",", ".")) / 6;
-			else if ("12 meses".equals(labels.get(x).getText()))
-				dy12 = Double.parseDouble(values.get(x).getText().replace("%", "").replace(",", ".")) / 12;
+		List<WebElement> prValues = driver.getElements(1).findElements(By.tagName("td"));
+		List<WebElement> dyValues = driver.getElements(2).findElements(By.tagName("td"));
+		for (int x = 0; x < labels.size() && x < dyValues.size(); x++) {
+			if ("Ãšltimo".equals(labels.get(x).getText())) {
+				pr1 = Double.parseDouble(prValues.get(x).getText().replace("R$ ", "").replace(",", "."));
+				dy1 = Double.parseDouble(dyValues.get(x).getText().replace("%", "").replace(",", "."));
+			} else if ("3 meses".equals(labels.get(x).getText())) {
+				pr3 = Double.parseDouble(prValues.get(x).getText().replace("R$ ", "").replace(",", "."));
+				dy3 = Double.parseDouble(dyValues.get(x).getText().replace("%", "").replace(",", ".")) / 3;
+			} else if ("6 meses".equals(labels.get(x).getText())) {
+				pr6 = Double.parseDouble(prValues.get(x).getText().replace("R$ ", "").replace(",", "."));
+				dy6 = Double.parseDouble(dyValues.get(x).getText().replace("%", "").replace(",", ".")) / 6;
+			} else if ("12 meses".equals(labels.get(x).getText())) {
+				pr12 = Double.parseDouble(prValues.get(x).getText().replace("R$ ", "").replace(",", "."));
+				dy12 = Double.parseDouble(dyValues.get(x).getText().replace("%", "").replace(",", ".")) / 12;
+			}
 		}
 
-		return String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%.4f\t%.4f\t%.4f\t%.4f", codigo, segmento, valorPatr, negociacoes, valorCota, dividendo, rentMensal, dy1, dy3, dy6, dy12);
+		return String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%.4f\t%.4f\t%.4f\t%.4f", codigo, segmento, valorPatr, negociacoes, valorCota, dividendo, rentMensal, pr1, pr3, pr6, pr12);
 	}
 }
